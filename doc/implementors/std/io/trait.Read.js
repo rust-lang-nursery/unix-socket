55542e8ca31159348b79f30e3ed68bfe9a6044d5
Steven Fackler <sfackler@gmail.com>
2015-03-04 07:22:14 +0000

    rebuild pages at ddb0e6f5df59638671c7ebc2a65a052d02e32161

--- doc/implementors/std/io/trait.Read.js
+++ doc/implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['unix_socket'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active deveopment and APIs are subject to tweaks fairly regularly'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='unix_socket/struct.UnixStream.html' title='unix_socket::UnixStream'>UnixStream</a>",];
+implementors['unix_socket'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active development and APIs are subject to tweaks fairly regularly'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='unix_socket/struct.UnixStream.html' title='unix_socket::UnixStream'>UnixStream</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
