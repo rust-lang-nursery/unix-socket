9eb372095107a5a3853cc1159666544df012273f
Steven Fackler <sfackler@gmail.com>
2015-03-14 05:20:22 +0000

    rebuild pages at e63800177677b25bbfd22058628ccf8bba5b1943

---
+++ doc/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['unix_socket'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='unix_socket/enum.AddressKind.html' title='unix_socket::AddressKind'>AddressKind</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='unix_socket/struct.SocketAddr.html' title='unix_socket::SocketAddr'>SocketAddr</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
