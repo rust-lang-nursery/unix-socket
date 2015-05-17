9cd1e478e5577dce9a49066d64dab1006e3f2947
Steven Fackler <sfackler@gmail.com>
2015-05-17 06:47:33 +0000

    rebuild pages at 3a01af1120e8a6c7381df37bdd2586dbfd352a44

--- doc/implementors/core/clone/trait.Clone.js
+++ doc/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['unix_socket'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='unix_socket/enum.AddressKind.html' title='unix_socket::AddressKind'>AddressKind</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='unix_socket/struct.SocketAddr.html' title='unix_socket::SocketAddr'>SocketAddr</a>",];
+implementors['unix_socket'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='unix_socket/enum.AddressKind.html' title='unix_socket::AddressKind'>AddressKind</a>&lt;'a&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='unix_socket/struct.SocketAddr.html' title='unix_socket::SocketAddr'>SocketAddr</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/marker/trait.Copy.js
+++ doc/implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['unix_socket'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='unix_socket/enum.AddressKind.html' title='unix_socket::AddressKind'>AddressKind</a>",];
+implementors['unix_socket'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='unix_socket/enum.AddressKind.html' title='unix_socket::AddressKind'>AddressKind</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
