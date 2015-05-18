3d479e2229361bb6b5709d8e03aa5b25e8c34c9f
Steven Fackler <sfackler@gmail.com>
2015-05-18 04:13:28 +0000

    rebuild pages at 60b75116c4d9df61c10044df006ea445ed42772d

---
+++ doc/master/implementors/core/iter/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['unix_socket'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='unix_socket/struct.UnixListener.html' title='unix_socket::UnixListener'>UnixListener</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
