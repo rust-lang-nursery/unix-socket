5916d01fb345beb0fafc8e82071e706adfd2eee4
Steven Fackler <sfackler@gmail.com>
2015-05-18 04:14:06 +0000

    rebuild pages at 6effeec1d82101d6fda59449049c9f380aeba104

--- doc/master/implementors/core/iter/trait.IntoIterator.js
+++ doc/master/implementors/core/iter/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['unix_socket'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='unix_socket/struct.UnixListener.html' title='unix_socket::UnixListener'>UnixListener</a>",];
+implementors['unix_socket'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='unix_socket/struct.UnixListener.html' title='unix_socket::UnixListener'>UnixListener</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/master/implementors/core/iter/trait.Iterator.js
+++ doc/master/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['unix_socket'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='unix_socket/struct.Incoming.html' title='unix_socket::Incoming'>Incoming</a>&lt;'a&gt;",];
+implementors['unix_socket'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='unix_socket/struct.Incoming.html' title='unix_socket::Incoming'>Incoming</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
