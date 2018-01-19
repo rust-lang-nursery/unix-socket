6ed98d3314bb049caa3791ce994a36d91bc1ac33
Sebastian Weisgerber <weisgerber@cispa.saarland>
2018-01-19 13:34:07 +0100

    Removed code duplication

--- src/lib.rs
+++ src/lib.rs
@@ -691,24 +691,14 @@ impl UnixListener {
     ///
     /// let listener = UnixListener::bind("/path/to/the/socket").unwrap();
     /// ```
-    #[cfg(target_os="android")]
+
     pub fn bind<P: AsRef<Path>>(path: P) -> io::Result<UnixListener> {
         unsafe {
             let inner = try!(Inner::new(libc::SOCK_STREAM));
             let (addr, len) = try!(sockaddr_un(path));
-
+            #[cfg(target_os="android")]
             try!(cvt(libc::bind(inner.0, &addr as *const _ as *const _, len as i32)));
-            try!(cvt(libc::listen(inner.0, 128)));
-
-            Ok(UnixListener { inner: inner })
-        }
-    }
-    #[cfg(not(target_os="android"))]
-    pub fn bind<P: AsRef<Path>>(path: P) -> io::Result<UnixListener> {
-        unsafe {
-            let inner = try!(Inner::new(libc::SOCK_STREAM));
-            let (addr, len) = try!(sockaddr_un(path));
-
+            #[cfg(not(target_os="android"))]
             try!(cvt(libc::bind(inner.0, &addr as *const _ as *const _, len)));
             try!(cvt(libc::listen(inner.0, 128)));
 
@@ -941,23 +931,14 @@ impl UnixDatagram {
     ///
     /// let socket = UnixDatagram::bind("/path/to/my/socket").unwrap();
     /// ```
-    #[cfg(target_os="android")]
+
     pub fn bind<P: AsRef<Path>>(path: P) -> io::Result<UnixDatagram> {
         unsafe {
             let inner = try!(Inner::new(libc::SOCK_DGRAM));
             let (addr, len) = try!(sockaddr_un(path));
-
+            #[cfg(target_os="android")]
             try!(cvt(libc::bind(inner.0, &addr as *const _ as *const _, len as i32)));
-
-            Ok(UnixDatagram { inner: inner })
-        }
-    }
-    #[cfg(not(target_os="android"))]
-     pub fn bind<P: AsRef<Path>>(path: P) -> io::Result<UnixDatagram> {
-        unsafe {
-            let inner = try!(Inner::new(libc::SOCK_DGRAM));
-            let (addr, len) = try!(sockaddr_un(path));
-
+            #[cfg(not(target_os="android"))]
             try!(cvt(libc::bind(inner.0, &addr as *const _ as *const _, len)));
 
             Ok(UnixDatagram { inner: inner })
